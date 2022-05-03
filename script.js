40b59117e62abf6896974e21b91cab41056b01e4
omdagade <omdagade13@gmail.com>
2022-05-03 15:03:43 +0530

    search enable

--- script.js
+++ script.js
@@ -46,3 +46,32 @@ window.onscroll = () =>{
     },
   });
   
+
+
+
+
+
+
+  function searchbook() {
+    let filter = document.getElementById("myInput").value.toUpperCase();
+    let ul = document.getElementById("myUL");
+    let li = ul.getElementsByTagName("li");
+    for (var i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+        let textvalue = a.textContent || a.innerHTML;
+        if (textvalue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
